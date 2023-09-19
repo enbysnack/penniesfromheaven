2641c77e288adc31d567baacb39354ddb177fd87
Kodi <144355274+enbysnack@users.noreply.github.com>
2023-09-19 09:54:12 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+def convert_pennies(amount):
+    dollars = amount // 100
+    amount %= 100
+    quarters = amount // 25
+    amount %= 25
+    dimes = amount // 10
+    amount %= 10
+    nickels = amount // 5
+    pennies = amount % 5
+
+    return dollars, quarters, dimes, nickels, pennies
+
+try:
+    pennies_input = int(input("Enter the amount in pennies: "))
+    if pennies_input < 0:
+        print("Please enter a non-negative number of pennies.")
+    else:
+        dollars, quarters, dimes, nickels, pennies = convert_pennies(pennies_input)
+
+        print(f"Dollars: {dollars}")
+        print(f"Quarters: {quarters}")
+        print(f"Dimes: {dimes}")
+        print(f"Nickels: {nickels}")
+        print(f"Pennies: {pennies}")
+except ValueError:
+    print("Please enter a valid number of pennies.")
